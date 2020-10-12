80b0c0cd1136c3f19387b20099bc9894c74169e6
CyberHedgehog <reg.dobr@live.ru>
2020-10-12 13:44:14 +0300

    Added locales

---
+++ server/locales/en.js
@@ -0,0 +1,25 @@
+export default {
+  translation: {
+    appName: 'TaskManager',
+    views: {
+      layouts: {
+        main: {
+          logOut: 'Logout',
+          head: 'Welcome!',
+          footer: {
+            text: 'Dmitriy Dobrenkiy',
+            ghLink: 'My Github',
+          },
+        },
+      },
+      pages: {
+        startPage: {
+          notLoggedButton: 'Get started',
+          loggedButton: 'Go to tasks!',
+          greeting: 'Welcome!',
+          text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        },
+      },
+    },
+  },
+};
