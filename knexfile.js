80795fe802a4ee39dd921b8ee8bc43e31342e69d
CyberHedgehog <reg.dobr@live.ru>
2020-10-13 10:18:51 +0300

    knexfile replaced

---
+++ knexfile.js
@@ -0,0 +1,38 @@
+// Update with your config settings.
+const migrations = {
+  directory: './server/migrations',
+};
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './dev.sqlite',
+    },
+    migrations,
+    useNullAsDefault: true,
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user: 'username',
+      password: 'password',
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations,
+    useNullAsDefault: true,
+  },
+
+  test: {
+    client: 'sqlite3',
+    connection: ':memory:',
+    migrations: { directory: './server/migrations' },
+    useNullAsDefault: true,
+  },
+};
