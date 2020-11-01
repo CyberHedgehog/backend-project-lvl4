0e685dff8917576c7db91cc8e9a8832d8342bb95
CyberHedgehog <reg.dobr@live.ru>
2020-11-01 14:08:38 +0000

    ip changed

--- server/bin/server.js
+++ server/bin/server.js
@@ -3,4 +3,4 @@ import getApp from '../index';
 const port = process.env.PORT || 3000;
 const app = getApp();
 console.log(process.env);
-app.listen(port, '0.0.0.0').catch(console.log);
+app.listen(port, '::').catch(console.log);
