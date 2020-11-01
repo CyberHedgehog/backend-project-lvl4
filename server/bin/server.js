2407b002aca83ba57d6a40932e754ad8dcef0b88
CyberHedgehog <reg.dobr@live.ru>
2020-11-01 14:03:21 +0000

    checking ip

--- server/bin/server.js
+++ server/bin/server.js
@@ -2,5 +2,5 @@ import getApp from '../index';
 
 const port = process.env.PORT || 3000;
 const app = getApp();
-console.log(port);
+console.log(process.env);
 app.listen(port, '0.0.0.0').catch(console.log);
